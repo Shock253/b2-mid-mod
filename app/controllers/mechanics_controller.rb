a0ba7fc5891421bcf6df5e3b1fea4b5dc077c98e
Ethan Hocking <eshocking253@gmail.com>
2020-04-03 11:45:51 -0600

    Add update action to mechanics controller

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,9 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def update
+    MechanicRide.create(mechanic_id: params[:id], ride_id: params[:ride_id])
+    redirect_to "/mechanics/#{params[:id]}"
+  end
 end
