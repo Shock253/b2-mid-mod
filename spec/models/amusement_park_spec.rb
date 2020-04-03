786a6fbeed5ec769a03447fcdc03acc5cdac1181
Ethan Hocking <eshocking253@gmail.com>
2020-04-03 11:08:12 -0600

    Add spec for average_thrill_rating helper method

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,25 @@ RSpec.describe AmusementPark, type: :model do
   describe 'relationships' do
     it { should have_many :rides }
   end
+
+  describe "#average_thrill_rating" do
+    it "returns the average thrill rating of all it's rides" do
+      amusement_park = AmusementPark.create(name: "Hershey Park",
+                                            admission_price: "50.00")
+
+      amusement_park.rides.create(          name: "Lightning Racer",
+                                            thrill_rating: 10)
+
+      amusement_park.rides.create(          name: "Storm Runner",
+                                            thrill_rating: 5)
+
+      amusement_park.rides.create(          name: "The Great Bear",
+                                            thrill_rating: 2)
+
+      # since 17 divided by 3 is a repeating decimal,
+      # I'm using math in my test just to get the exact
+      # value that will return in the program
+      expect(amusement_park.average_thrill_rating.to_f).to eq(17.0/3)
+    end
+  end
 end
