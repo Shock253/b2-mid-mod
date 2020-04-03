a92a92da8e7064bcbb59d28dd65bfa9ac72ee8a2
Ethan Hocking <eshocking253@gmail.com>
2020-04-03 11:52:23 -0600

    Add spec for id verification on the mechanic show page form

--- spec/mechanics/mechanics_show_spec.rb
+++ spec/mechanics/mechanics_show_spec.rb
@@ -48,6 +48,39 @@ RSpec.describe "As a user,", type: :feature do
       expect(page).to have_current_path("/mechanics/#{mechanic.id}")
       expect(page).to have_content("Lightning Racer")
     end
+
+    it "If I enter an invalid id into the ride id form,
+    then I see a flash message telling me that the ride doesn't exist" do
+      mechanic = Mechanic.create(           name: "Kara Smith",
+                                            years_experience: 11)
+
+      amusement_park = AmusementPark.create(name: "Hershey Park",
+                                            admission_price: "50.00")
+
+      ride_1 = amusement_park.rides.create( name: "The Frog Hopper",
+                                            thrill_rating: 10)
+
+      ride_2 = amusement_park.rides.create( name: "Fahrenheit",
+                                            thrill_rating: 5)
+
+      ride_3 = amusement_park.rides.create( name: "The Kiss Raise",
+                                            thrill_rating: 2)
+
+      ride_4 = amusement_park.rides.create( name: "Lightning Racer",
+                                            thrill_rating: 5)
+
+      MechanicRide.create(mechanic: mechanic, ride: ride_1)
+      MechanicRide.create(mechanic: mechanic, ride: ride_2)
+      MechanicRide.create(mechanic: mechanic, ride: ride_3)
+
+      visit "/mechanics/#{mechanic.id}"
+
+      fill_in "Ride", with: "yeet"
+      click_button "Submit"
+
+      expect(page).to have_current_path("/mechanics/#{mechanic.id}")
+      expect(page).to have_content("Could not find ride of given ID")
+    end
   end
 end
 # Story 3
